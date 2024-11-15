9df40f77bb4efe63f2de09cd249ee56cbbaefe55
brunofrezende <brunofrarez@yahoo.com>
2024-11-15 11:40:24 -0300

    Commit novo

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 
 
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, send_from_directory
 import json
+import ast
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'BRUNOMATSOLO'
 
-
+logado = False
 
 @app.route('/')
 def home():
+    global logado 
+    logado = False
     return render_template('login.html')
+
+@app.route('/adm')
+def adm():
+    if logado == True:
+        with open('usuarios.json') as usuariosTemp:
+            usuarios = json.load(usuariosTemp)
+            
+        return render_template("administrador.html",usuarios=usuarios)
+    if logado == False:
+        return redirect('/')
+    
+@app.route('/usuarios')
+def usuarios():
+    if logado == True:
+        arquivo = []
+        for documento in os.listdir('../site_bruno_rezende/arquivos'):
+            arquivo.append(documento)
+
+        return render_template("usuario.html", arquivos=arquivo)
+    else:
+        return redirect('/')
+    
 
 
 @app.route('/login', methods=['POST'])
 def login():
+    global logado 
 
     nome = request.form.get('nome')
     senha = request.form.get('senha')
@@ -24,13 +51,78 @@ def login():
         cont = 0
         for usuario in usuarios:
             cont += 1
+
+            if nome == 'adm' and senha == '000':
+                logado = True
+                return redirect('/adm')
+
             if usuario['nome'] == nome and usuario['senha'] == senha:
-                return render_template("usuario.html")
+                logado = True
+                return redirect('/usuarios')
             
             if cont >= len(usuarios):
                 flash('USUÁRIO INVÁLIDO')
                 return redirect("/")
 
+@app.route('/cadastrarUsuario', methods=['POST'])
+def cadastrarUsuario():
+    global logado
+    user = []
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+    user = [
+        {
+            "nome": nome,
+            "senha": senha
+        }
+    ]
+    with open('usuarios.json') as usuariosTemp:
+        usuarios = json.load(usuariosTemp)
+
+    usuarioNovo = usuarios + user 
+
+    with open('usuarios.json', 'w') as gravarTemp:
+        json.dump(usuarioNovo, gravarTemp, indent=4 )
+    logado = True
+    flash(F'{nome} CADASTRADO!')
+    return redirect('/adm')
+
+@app.route('/excluirUsuario', methods=['POST'])
+def excluirUsuario():
+    global logado
+    logado = True
+    usuario = request.form.get('usuarioPexcluir')
+    usuarioDict = ast.literal_eval(usuario)
+    nome = usuarioDict['nome']
+    with open('usuarios.json') as usuariosTemp:
+        usuariosJson = json.load(usuariosTemp)
+        for c in usuariosJson:
+            if c == usuarioDict:
+                usuariosJson.remove(usuarioDict)
+                with open('usuarios.json', 'w') as usuarioAexcluir:
+                    json.dump(usuariosJson, usuarioAexcluir, indent=4)
+
+    flash(F'{nome} EXCLUÍDO')
+    return redirect('/adm')
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    global logado
+    logado = True
+
+    arquivo = request.files.get('documento')
+    nome_arquivo = arquivo.filename.replace(" ", "-")
+    arquivo.save(os.path.join('../site_bruno_rezende/arquivos/', nome_arquivo))
+
+    flash('Arquivo enviado')
+    return redirect('/adm')
+
+@app.route('/download', methods=['POST'])
+def download():
+    nomeArquivo = request.form.get('arquivosParaDownload')
+
+    return send_from_directory('arquivos', nomeArquivo, as_attachment=True)
 
 
 
